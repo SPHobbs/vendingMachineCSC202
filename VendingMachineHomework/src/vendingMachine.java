77e3d98b57fe4b46c5bdd7cddcbe0b5971f608fe
sph22413 <sph22413@AN112514-CT103.nvstu.edu>
2015-09-09 16:18:07 -0400

    This is the final version of the program

--- VendingMachineHomework/src/vendingMachine.java
+++ VendingMachineHomework/src/vendingMachine.java
@@ -5,17 +5,18 @@ import java.io.PrintStream;
 
 public class vendingMachine {
 	//This is the method that handles the exceptions as well as the printing and stocking of the machine
+	static int snacksDispensed=0;
+	static int drinksDispensed=0;
+	static int totalMoney=0;
 	
-	
-	
-	
-	
-	public static void money(){
-		//This is the method that handles all the processes that have to do with money
-		
-	}
 	public static void main(String[]args){
 		//This method will create the customers and determine the order for which they arrive
+		System.out.println("the machine has turned on for the day");
+		
+		//int snacksDispensed=0;
+		//int drinksDispensed=0;
+		//int totalMoney=0;
+		
 		int i=0;
 		double time = 0.0;
 		
@@ -30,21 +31,29 @@ public class vendingMachine {
 				int randomSelection= (int) (10*Math.random());
 				
 				System.out.println("The user has entered $" + randomMoney);
+				totalMoney+=randomMoney;
 				
 				if(randomMachine<5){
 					System.out.println("The user has selected the snack machine");
+					snacksDispensed+=1;
 					dispenser.snacksDispenser();
 				}
 				else{
 					System.out.println("The user has selected the drinks machine");
+					drinksDispensed+=1;
 					dispenser.drinksDispenser();
 				}
 				System.out.println("The time is " + time);
+				for(int j=0;j<3;j++){
+					System.out.println();
+				}
 				time += randomTime;
 				i++;
 			}
 			else{
-				System.out.println("The machine has shut for the day.")
+				System.out.println("The machine has shut off for the day.");
+				
+				break;
 			}
 			
 			
@@ -55,7 +64,9 @@ public class vendingMachine {
 		//This is the method that handles all the file and text printing
 		try {
 			PrintStream out = new PrintStream(new FileOutputStream("CustomerReceipt.txt"));
-			
+				out.println("The total number of snacks dispensed were " + snacksDispensed);
+				out.println("The total number of drinks dispensed were " + drinksDispensed);
+				out.println("The total amount of money deposited into the machine is " + totalMoney);
 			
 			
 		} catch (FileNotFoundException e) {
